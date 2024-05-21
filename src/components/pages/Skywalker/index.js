83a758e8ca4a2905ebb9957f6ae8a04af968f148
frank <frankjr212711@gmail.com>
2024-05-21 15:14:00 +0000

    water-features-added

--- src/components/pages/Skywalker/index.js
+++ src/components/pages/Skywalker/index.js
@@ -56,9 +56,9 @@ const Content = () => {
               </p>
 
               <nav>
-                <li onClick={() => setCurrent(0)}>Sachet Water</li>
-                <li onClick={() => setCurrent(1)}> Dispenser Bottled</li>
-                <li onClick={() => setCurrent(2)}>Dispensers</li>
+                <li onClick={() => setCurrent(0)}>Sachet Waters</li>
+                <li onClick={() => setCurrent(1)}> Water Refill Bottles</li>
+                <li onClick={() => setCurrent(2)}>Water Dispensers</li>
               </nav>
 
               <br />
@@ -75,26 +75,18 @@ const Content = () => {
 
         
             <article className={current === 0 ? "active" : 'gone'} >
-                  <h2>Sachet Water Production & Sales</h2>
-                  <img src="/images/sky-img-031.jpg" alt="sachet_img" />
+                  <h2>Sachet Waters</h2>
+                  <img src="/images/sky-img-01.jpg" alt="sachet_img" />
                   <p>
-                    Lorem ipsum dolor siti amet consectetur adipisicing elit,
-                    Dignissiomos similique minima tempora corrupti neque eos
-                    dolorum et optio voluptatem ipsam recusandae debitis
-                    aspernatur expedita, dolorem eveniet esse fuga voluptatibus,
-                    quae laudantium, odit repellendus consequuntur architecto
-                    facere quis. Odio queae alias sequi iusto laborum,
-                    voluptatum ratione maiores laudantium, nihil debitess ipsum.
+                  Water sachets or sachet water is a common form of selling pre-filtered or sanitized water in plastic, heat sealed bags in parts of the global south, and are especially popular in Africa. Water sachets are cheaper to produce than plastic bottles, and easier to transport. In some countries, water vendors refer to sachet water as "pure water".
+
+
                   </p>
 
                   <p>
-                    Lorem ipsum dolor siti amet consectetur adipisicing elit,
-                    Dignissiomos similique minima tempora corrupti neque eos
-                    dolorum et optio voluptatem ipsam recusandae debitis
-                    aspernatur expedita, dolorem eveniet esse fuga voluptatibus,
-                    quae laudantium, odit repellendus consequuntur architecto
-                    facere quis. Odio queae alias sequi iusto laborum,
-                    voluptatum ratione maiores laudantium, nihil debitess ipsum.
+                  High demand, and poor collection of waste from consumers, has resulted in significant plastic pollution and waste from sachets throughout the West Africa. Accumulation of sachets frequently causes blocked stormwater drainage, and other issues. Some countries, such as Senegal, have banned disposable sachets.
+
+Because sachets are frequently filled in small and often unregulated facilities, inadequate sanitary conditions can occasionally result in disease or contamination. However, in countries like Ghana consumers still prefer that access over other forms of venders, with a perception of lower risk. This form of water distribution provides vital access to water in communities that otherwise wouldn't have it. However, some scholars have identified this method of distribution as having potential human rights and social justice issues, limiting the right to water and sanitation.
                   </p>
                 </article>
 
@@ -102,67 +94,54 @@ const Content = () => {
                   <ul>
                     <li>
                     <p>
-                        <strong>Published on:</strong>
+                        <strong>Item:</strong>
                       </p>
-                      <p>May 6th 2022</p>
+                      <p>Sachet Water</p>
                     </li>
                     <li>
                       <p>
-                        <strong>In the category:</strong>
+                        <strong>Type:</strong>
                       </p>
-                      <p>Games and Consoles</p>
+                      <p>Drinking water</p>
                     </li>
                     <li>
                       <p>
-                        <strong>Written by:</strong>
+                        <strong>Capacity:</strong>
                       </p>
-                      <p>Mario</p>
+                      <p>500ml</p>
                     </li>
                     <li>
                       <p>
-                        <strong>Published on:</strong>
+                        <strong>Size:</strong>
                       </p>
-                      <p>May 6th 2022</p>
+                      <p>320mm*140mm*0.0.5mm</p>
+                    </li>
+                    
+                    <li>
+                      <p>
+                        <strong>HPC Level:</strong>
+                      </p>
+                      <p>86.7%</p>
                     </li>
                     <li>
                       <p>
-                        <strong>In the category:</strong>
+                        <strong>Weight:</strong>
                       </p>
-                      <p>Games and Consoles</p>
+                      <p>N.W.: 15-20kg/roll, 2 rolls/carton</p>
                     </li>
-                    <li>
-                      <p>
-                        <strong>Written by:</strong>
-                      </p>
-                      <p>Mario</p>
-                    </li>
+                   
                   </ul>
                 </aside>
            
-          
-
-        
                 <article className={current === 1 ? "active" : 'gone'} >
-                  <h2>Dispenser Bottles</h2>
+                  <h2>Refill Water Bottles</h2>
                   <img src="/images/sky-img-03.png" alt="sachet_img" />
                   <p>
-                    Lorem ipsum dolor siti amet consectetur adipisicing elit,
-                    Dignissiomos similique minima tempora corrupti neque eos
-                    dolorum et optio voluptatem ipsam recusandae debitis
-                    aspernatur expedita, dolorem eveniet esse fuga voluptatibus,
-                    quae laudantium, odit repellendus consequuntur architecto
-                    facere quis. Odio queae alias sequi iusto laborum,
-                    voluptatum ratione maiores laudantium, nihil debitess ipsum.
+                  One of our best-selling products is our 15-liter spring water bottles. Customers adore the crisp, refreshing flavor of our water and keep coming back for more bottles. 
                   </p>
 
                   <p>
-                    Lorem ipsum dolor siti amet consectetur adipisicing elit,
-                    Dignissiomos similique minima tempora corrupti neque eos
-                    dolorum et optio voluptatem ipsam recusandae debitis
-                    aspernatur expedita, dolorem eveniet esse fuga voluptatibus,
-                    quae laudantium, odit repellendus consequuntur architecto
-                    facere quis. Odio queae alias sequi iusto laborum,
-                    voluptatum ratione maiores laudantium, nihil debitess ipsum.
+                  We are pleased to announce that our bottles are completely recyclable and contain 30% less plastic than our previous bottles. They contain no BPA at all.
                   </p>
                 </article>
 
@@ -170,40 +149,30 @@ const Content = () => {
                   <ul>
                     <li>
                       <p>
-                        <strong>Published on:</strong>
+                        <strong>Item:</strong>
                       </p>
-                      <p>May 6th 2022</p>
+                      <p>Refill Bottle</p>
                     </li>
                     <li>
                       <p>
-                        <strong>In the category:</strong>
+                        <strong>Type:</strong>
                       </p>
-                      <p>Games and Consoles</p>
+                      <p>Bottle with Hole Handle</p>
                     </li>
                     <li>
                       <p>
-                        <strong>Written by:</strong>
+                        <strong>Dimensions:</strong>
                       </p>
-                      <p>Mario</p>
+                      <p>25.4x25.5 x 45.72 cm</p>
                     </li>
+                    
                     <li>
                       <p>
-                        <strong>Published on:</strong>
+                        <strong>Capacity:</strong>
                       </p>
-                      <p>May 6th 2022</p>
+                      <p>18.5 Litres(5 gallons)</p>
                     </li>
-                    <li>
-                      <p>
-                        <strong>In the category:</strong>
-                      </p>
-                      <p>Games and Consoles</p>
-                    </li>
-                    <li>
-                      <p>
-                        <strong>Written by:</strong>
-                      </p>
-                      <p>Mario</p>
-                    </li>
+                    
                   </ul>
                 </aside>
        
@@ -211,25 +180,17 @@ const Content = () => {
         
               <article className={current === 2 ? "active" : 'gone'} >
                   <h2>Dispenser Refridgerators</h2>
-                  <img src="/images/dispense-1.jpg" alt="sachet_img" />
+                  <img src="/images/sky-img-02.jpg" alt="sachet_img" />
                   <p>
-                    Lorem ipsum dolor siti amet consectetur adipisicing elit,
-                    Dignissiomos similique minima tempora corrupti neque eos
-                    dolorum et optio voluptatem ipsam recusandae debitis
-                    aspernatur expedita, dolorem eveniet esse fuga voluptatibus,
-                    quae laudantium, odit repellendus consequuntur architecto
-                    facere quis. Odio queae alias sequi iusto laborum,
-                    voluptatum ratione maiores laudantium, nihil debitess ipsum.
+                  A water dispenser, known as water cooler (if used for cooling only), is a machine that dispenses and often also cools or heats up water with a refrigeration unit. It is commonly located near the restroom due to closer access to plumbing. A drain line is also provided from the water cooler into the sewer system.
+
+
                   </p>
 
                   <p>
-                    Lorem ipsum dolor siti amet consectetur adipisicing elit,
-                    Dignissiomos similique minima tempora corrupti neque eos
-                    dolorum et optio voluptatem ipsam recusandae debitis
-                    aspernatur expedita, dolorem eveniet esse fuga voluptatibus,
-                    quae laudantium, odit repellendus consequuntur architecto
-                    facere quis. Odio queae alias sequi iusto laborum,
-                    voluptatum ratione maiores laudantium, nihil debitess ipsum.
+                  Water dispensers come in a variety of form factors, ranging from wall-mounted to bottle filler water dispenser combination units, to bi-level units and other formats. They are generally broken up into two categories: point-of-use (POU) water dispensers and bottled water dispensers. POU water dispensers are connected to a water supply, while bottled water dispensers require delivery (or self-pick-up) of water in large bottles from vendors. Bottled water dispensers can be top-mounted or bottom-loaded, depending on the design of the model.
+
+Bottled water dispensers typically use 11- or 22-liter (5- or 10-gallon) dispensers commonly found on top of the unit. Pressure coolers are a subcategory of water dispensers encompassing drinking water fountains and direct-piping water dispensers. Water cooler may also refer to a primitive device for keeping water cool
                   </p>
                 </article>
 
@@ -237,39 +198,41 @@ const Content = () => {
                   <ul>
                     <li>
                       <p>
-                        <strong>Published on:</strong>
+                        <strong>Item:</strong>
                       </p>
-                      <p>May 6th 2022</p>
+                      <p>Water Dispenser</p>
                     </li>
                     <li>
                       <p>
-                        <strong>In the category:</strong>
+                        <strong>Type:</strong>
                       </p>
-                      <p>Games and Consoles</p>
+                    
+                      <p>* Bottled water dispensers </p>
+                   
                     </li>
                     <li>
                       <p>
-                        <strong>Written by:</strong>
+                        <strong>Capacity:</strong>
                       </p>
-                      <p>Mario</p>
+                      <p>18.5 Litre </p>
+                    </li>
+                    
+                    <li>
+                      <p>
+                        <strong>Size:</strong>
+                      </p>
+                      <p><strong>Depth:</strong> 372mm</p> 
+                      <p><strong>Height:</strong> 1003mm</p> 
+                      <p><strong>Width:</strong> 318mm</p> 
                     </li>
                     <li>
                       <p>
-                        <strong>Published on:</strong>
+                        <strong>Weight:</strong>
                       </p>
-                      <p>May 6th 2022</p>
-                    </li>
-                    <li>
-                      <p>
-                        <strong>In the category:</strong>
-                      </p>
-                      <p>Games and Consoles</p>
-                    </li>
-                    <li>
-                      <p>
-                        <strong>Written by:</strong>
-                      </p>
-                      <p>Mario</p>
+                      <p><strong>Small water bottle:</strong> 8-12 oz: 0.4 to 0.6 pounds (approximately)</p> 
+                      <p><strong>Medium water bottle:</strong> 16-20 oz: 0.8 to 1 pounds (approximately)</p> 
+                      <p><strong>Large water bottle:</strong> 24-32 oz: 1.2 to 1.5 pounds (approximately)</p> 
+                    
                     </li>
                   </ul>
                 </aside>
